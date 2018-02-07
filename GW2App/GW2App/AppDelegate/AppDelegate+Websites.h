27a843fd91a52898792bf44df00c5bd41f8f6757
min_liu <ihyun628@gmail.com>
2018-02-07 14:22:33 +0800

    [modify][GW2App] 1.在 AppDelegate 下 category 一個 Websites文件

---
+++ GW2App/GW2App/AppDelegate/AppDelegate+Websites.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate+Websites.h
+//  GW2App
+//
+//  Created by min_liu on 2018/2/7.
+//  Copyright © 2018年 min_liu. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate (Websites)
+
+
+/* 與網站連線 */
+-(void) setWebRequestUrl;
+@end
--- GW2App/GW2App/AppDelegate/AppDelegate.m
+++ GW2App/GW2App/AppDelegate/AppDelegate.m
@@ -10,6 +10,9 @@
 
 #import "MainViewController.h"
 #import "ViewControllerTabBar.h"
+
+//web link
+#import "AppDelegate+Websites.h"
 @interface AppDelegate ()
 
 @end
@@ -23,21 +26,21 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    
-    
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    
-    //tabBar
-    ViewControllerTabBar *tabBarContrller = [[ViewControllerTabBar alloc] init];
 
+    [self initWithViewControl];
     
-    self.window.rootViewController = tabBarContrller;
-
-    
-    [self.window makeKeyAndVisible];
+    [self setWebRequestUrl];
     return YES;
 }
 
+-(void) initWithViewControl{
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    //tabBar
+    ViewControllerTabBar *tabBarContrller = [[ViewControllerTabBar alloc] init];
+    self.window.rootViewController = tabBarContrller;
+    [self.window makeKeyAndVisible];
+}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
